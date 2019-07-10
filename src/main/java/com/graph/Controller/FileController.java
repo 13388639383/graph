1719c67c3e5e4757dfe9ab107813d35d09262c11
lenovo <893918821@qq.com>
2019-07-10 12:59:01 +0800

    graph1.1

--- src/main/java/com/graph/Controller/FileController.java
+++ src/main/java/com/graph/Controller/FileController.java
@@ -2,16 +2,17 @@ package com.graph.Controller;
 
 import com.graph.Domain.Result;
 import com.graph.Util.ResponseResult;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-
+@RestController
 public class FileController {
     @GetMapping("/getfile")
     public ResponseResult GetFile() {
         ResponseResult<Result> Result = new ResponseResult<>();
         try {
-            Runtime.getRuntime().exec("cmd /c start /b java");
             Result.setMessage("成功");
             Result.setData(null);
             Result.setStatus(ResponseResult.STATE_OK);
--- src/main/resources/static/js/getfile.js
+++ src/main/resources/static/js/getfile.js
@@ -2,15 +2,14 @@ $(function(){
     $('#getfile').click(function(){
             $.ajax({
                 url: '/getfile',
-                type: 'POST',
+                type: 'GET',
                 dataType:'json',
-                contentType: "application/json",
                 success:function(root){
                     if(root.status==1){
                         alert('识别成功！');
                     }
                     else{
-                        alert('识别失败,请联系管理员！');
+                        alert('识别失败！');
                     }
                 },
                 error:function(){
